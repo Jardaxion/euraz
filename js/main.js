dc4a9fdc840e12d0af37f70b23b5d271c45b2968
Jardaxion <bainthecreator21@gmail.com>
2022-11-16 02:39:43 +0700

    Мобилка

--- js/main.js
+++ js/main.js
@@ -3,16 +3,20 @@ $(document).ready(function(){
     $('.js-open-desktop-menu').on('click',function(e){
         e.preventDefault();
 
-        $('.header__menu').toggleClass("active");
-        $(this).toggleClass('reverse');
+        if($(window).width() > 960){
+            $('.header__menu:not(.mobile)').toggleClass("active");
+            $(this).toggleClass('reverse');
+        } else{
+            $('.header__menu.mobile').toggleClass("active");
+            $(this).toggleClass('reverse');
+        }
     })
 
-    $(document).on('click', (e) => {
-        let log = (e.target == document.querySelector('.header__link') || e.target == document.querySelector('.js-open-desktop-menu svg path'));
-        if(log && e.target == document.querySelector('.header__menu')){
-            $('.header__menu').removeClass("active");
-            $('.js-open-desktop-menu').removeClass('reverse');
-        }
+    //Октрытие закрытие меню на телефоне
+    $('.js-open-mobile-menu').on('click', function(e){
+        e.preventDefault();
+
+        openCloseMobMenu();
     })
 
     //Закрытие открытие вопроса в faq
@@ -53,6 +57,20 @@ $(document).ready(function(){
         prevArrow: '.questions__leftArrow',
         slidesToShow: 3,
         infinite: false,
+        responsive: [
+            {
+                breakpoint: 961,
+                settings: {
+                    slidesToShow: 2
+                },
+            },
+            {
+                breakpoint: 569,
+                settings: {
+                    slidesToShow: 1
+                },
+            }
+        ]
     })
 
     //Прокручиваемая строка с картиками
@@ -61,6 +79,73 @@ $(document).ready(function(){
         startVisible: true,
     })
 
+    //Модальные окна
+    //Открытие
+    $('.js-open-modal').on('click', function(e){
+        e.preventDefault();
+
+        if($('.js-open-desktop-menu').hasClass('reverse')){
+            openCloseMenu();
+        }
+
+        if($('.js-open-mobile-menu').hasClass('reverse')){
+            openCloseMobMenu();
+        }
+
+        openModal($(this).data('modal'));
+    })
+    //Закрытие
+    $('.js-close-modal').on('click', function(e){
+        e.preventDefault();
+
+        closeModal();
+    })
+    $('.modal').on('click', function(e){
+        e.preventDefault();
+
+        if(e.target === document.querySelector('.modal')){
+            closeModal();
+        }
+
+    })
+    //Переоткрытие
+    $('.js-reOpen-modal').on('click', function(e){
+        e.preventDefault();
+
+        reOpenModal($(this).data('modal'));
+    })
+
     //Адаптация
-    $('.service__catalog').height($('.services__bottom').height());
+    $(window).width() > 960 ? $('.service__catalog').height($('.services__bottom').height()) : false;
 })
+//Функции
+//Открытие-закртыие менюшки мобильной
+function openCloseMobMenu(){
+    $('.js-open-mobile-menu').toggleClass('reverse');
+    $('.header__mobileMenu-wrapper').fadeToggle();
+    $('body').toggleClass('noScroll');
+    if($('.header__menu.mobile').hasClass('active')){
+        $('.header__menu.mobile').removeClass('active');
+    }
+}
+
+//Модальные окна
+//Открытие
+function openModal(id){
+    $('.modal#'+id).addClass('active');
+    $('.modal__background').addClass('active');
+    $('body').addClass('noScroll');
+}
+
+//Закрытие
+function closeModal() {
+    $('.modal.active').removeClass('active');
+    $('.modal__background').removeClass('active');
+    $('body').removeClass('noScroll');
+}
+
+//Закрытие одного модального окна и открытие другого
+function reOpenModal(id) {
+    $('.modal.active').removeClass('active');
+    $('.modal#'+id).addClass('active');
+}
